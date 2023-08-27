1a3f8cd4bf64b01a6be2559f94d89352bc966a5a
ViniciusApBrito <vinicius.brito355@gmail.com>
2023-08-27 19:05:35 -0300

    close #1 - exercios iniciais

---
+++ test1.py
@@ -0,0 +1,4 @@
+name = input ('Type your name: ')
+age  = input ('Enter your age: ')
+weight = input ('Enter your weight: ')
+print(name, age, weight)
---
+++ test2.py
@@ -0,0 +1,4 @@
+dia = input('Digite o dia do seu nascimento: ')
+mes = input('Digite o mês de nascimento: ')
+ano = input('Digite o ano que nasceu: ')
+print('Você nasceu no dia', dia, 'de', mes, 'do ano de', ano,'.Correto?')
